ea37388dd9e9e2f666ec0604b1b15b196637a783
Sega-13 <segapatidar@gmail.com>
2024-08-20 17:21:39 +0530

    Created instance of commands.

--- Assets/Scripts/Input/InputService.cs
+++ Assets/Scripts/Input/InputService.cs
@@ -25,7 +25,7 @@ namespace Command.Input
 
         public void UpdateInputService()
         {
-            if(currentState == InputState.SELECTING_TARGET)
+            if (currentState == InputState.SELECTING_TARGET)
                 mouseInputHandler.HandleTargetSelection(targetType);
         }
 
@@ -48,7 +48,47 @@ namespace Command.Input
         public void OnTargetSelected(UnitController targetUnit)
         {
             SetInputState(InputState.EXECUTING_INPUT);
-           // GameService.Instance.PlayerService.PerformAction(selectedActionType, targetUnit);
+            // GameService.Instance.PlayerService.PerformAction(selectedActionType, targetUnit);
+            UnitCommand commandToProcess = CreateUnitCommand(targetUnit);
+            GameService.Instance.ProcessUintCommand(commandToProcess);
+        }
+        private CommandData CreateCommandData(UnitController targetUnit)
+        {
+            // Create CommandData with the necessary information for a UnitCommand.
+            // It includes the ActiveUnit's ID, TargetUnit's ID, ActivePlayer's ID, and the TargetPlayer's ID.
+            return new CommandData(
+                GameService.Instance.PlayerService.ActiveUnitID,
+                targetUnit.UnitID,
+                GameService.Instance.PlayerService.ActivePlayerID,
+                targetUnit.Owner.PlayerID
+            );
+        }
+        private UnitCommand CreateUnitCommand(UnitController targetUnit)
+        {
+            // Create the necessary CommandData based on the target unit.
+            CommandData commandData = CreateCommandData(targetUnit);
+
+            // Based on the selected command type, create and return the corresponding UnitCommand.
+            switch (selectedCommandType)
+            {
+                case CommandType.Attack:
+                    return new AttackCommand(commandData);
+                case CommandType.Heal:
+                    return new HealCommand(commandData);
+                case CommandType.AttackStance:
+                    return new AttackStanceCommand(commandData);
+                case CommandType.Cleanse:
+                    return new CleanseCommand(commandData);
+                case CommandType.BerserkAttack:
+                    return new BerserkAttackCommand(commandData);
+                case CommandType.Meditate:
+                    return new MeditateCommand(commandData);
+                case CommandType.ThirdEye:
+                    return new ThirdEyeCommand(commandData);
+                default:
+                    // If the selectedCommandType is not recognized, throw an exception.
+                    throw new System.Exception($"No Command found of type: {selectedCommandType}");
+            }
         }
     }
 }
